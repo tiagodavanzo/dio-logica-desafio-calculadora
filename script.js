78f09c2c93f8cddfbc367d308d0b596322983f92
tiagodavanzo <tiago.davanzo@gmail.com>
2023-08-23 06:32:56 -0300

    Código do desafio

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 console.log("Calculadora de Partidas Rankeadas");
+
+function getBalance(wins, losses) {
+  return wins - losses;
+}
+
+function getLevel(balance) {
+  switch (true) {
+    case balance <= 10:
+      return "Ferro";
+    case balance >= 11 && balance <= 20:
+      return "Bronze";
+    case balance >= 21 && balance <= 50:
+      return "Prata";
+    case balance >= 51 && balance <= 80:
+      return "Ouro";
+    case balance >= 81 && balance <= 90:
+      return "Diamante";
+    case balance >= 91 && balance <= 100:
+      return "Lendário";
+    case balance >= 101:
+      return "Imortal";
+  }
+}
+
+let balance = getBalance(500, 30);
+let level = getLevel(balance);
+
+console.log(`O Herói tem de saldo de ${balance} está no nível ${level}`);
